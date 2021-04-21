5757547ade7d73951d2e9bd7441b1e397fcc9141
Kamil Binko <kallenved@gmail.com.se>
2021-04-21 09:53:06 +0200

    minor changes to neural net display

--- UserControls/Models/NeuralNetDisplayViewModel.cs
+++ UserControls/Models/NeuralNetDisplayViewModel.cs
@@ -12,29 +12,35 @@ namespace UserControls.Models
         public ObservableCollection<PrimitiveShape> DisplayItems { get; set; }
         private readonly PrimitiveShapeValueProvider[][] _lineValueProviders;
         private readonly PrimitiveShapeValueProvider[][] _circleValueProviders;
+        private readonly NetworkInfo _networkInfo;
         private const double cRadius = 10;
         public NeuralNetDisplayViewModel(NetworkInfo networkInfo)
         {
             DisplayItems = new ObservableCollection<PrimitiveShape>();
             _lineValueProviders = new PrimitiveShapeValueProvider[networkInfo.Layers][];
             _circleValueProviders = new PrimitiveShapeValueProvider[networkInfo.Layers + 1][];
+            _networkInfo = networkInfo;
+            CreateConnections();
+        }
 
-            double offset = (Cons.cNetWidth - 2 * Cons.cNetWidthPadding) / networkInfo.Layers;
+        private void CreateConnections()
+        {
+            double offset = (Cons.cNetWidth - 2 * Cons.cNetWidthPadding) / _networkInfo.Layers;
             double start = Cons.cNetWidthPadding;
 
-            for (int i = 1; i < networkInfo.LayerSetup.Length; i++)
+            for (int i = 1; i < _networkInfo.LayerSetup.Length; i++)
             {
-                CreateConnections(start, start + offset, networkInfo.LayerSetup[i - 1], networkInfo.LayerSetup[i], i - 1);
+                CreateConnections(start, start + offset, _networkInfo.LayerSetup[i - 1], _networkInfo.LayerSetup[i], i - 1);
                 start += offset;
             }
 
-            offset = (Cons.cNetWidth - 2 * Cons.cNetWidthPadding) / networkInfo.Layers;
+            offset = (Cons.cNetWidth - 2 * Cons.cNetWidthPadding) / _networkInfo.Layers;
             start = Cons.cNetWidthPadding;
 
 
-            for (int i = 0; i < networkInfo.LayerSetup.Length; i++)
+            for (int i = 0; i < _networkInfo.LayerSetup.Length; i++)
             {
-                CreateNeuronLayer(start, networkInfo.LayerSetup[i], i);
+                CreateNeuronLayer(start, _networkInfo.LayerSetup[i], i);
                 start += offset;
             }
         }
@@ -88,15 +94,15 @@ namespace UserControls.Models
             }
         }
 
-    public void OnResultsCalculated(IList<double[]> results)
-    {
-        for (int i = 0; i < results.Count; i++)
+        public void OnResultsCalculated(IList<double[]> results)
         {
-            for (int j = 0; j < results[i].Length; j++)
+            for (int i = 0; i < results.Count; i++)
             {
-                _circleValueProviders[i][j].Value = results[i][j];
+                for (int j = 0; j < results[i].Length; j++)
+                {
+                    _circleValueProviders[i][j].Value = results[i][j];
+                }
             }
         }
     }
-}
 }
