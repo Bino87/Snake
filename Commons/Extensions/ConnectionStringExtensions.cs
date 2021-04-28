be2eb264db5f80f4e3dc0a53b7b2cd234e567012
Kamil Binko <kallenved@gmail.com>
2021-04-28 12:17:57 +0200

    connection string 2

--- Commons/Extensions/ConnectionStringExtensions.cs
+++ Commons/Extensions/ConnectionStringExtensions.cs
@@ -8,7 +8,7 @@ namespace Commons.Extensions
         public static string GetConnectionString(this ConnectionStringSettingsCollection collection)
         {
 
-            //This is super ugly, gotta do this if i want to keep working on 2 computers. 
+            //This is super ugly, gotta do this if i want to keep working on 2 computers. Or keep changing the connection string in app.config.
             return collection[Environment.MachineName switch
                     {
                         "DESKTOP-D40UEJC" => "Home",
